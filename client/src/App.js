93d33e1f054572e95e53b047719807471099e4bf
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-29 23:28:58 -0400

    navbar updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import NavBar from './components/navbar';
 import Container from './components/container';
