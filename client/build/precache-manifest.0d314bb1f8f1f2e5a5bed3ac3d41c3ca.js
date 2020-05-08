d164a52c224f8cef5954d6340b06f6ffa489ea4f
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-08 19:02:54 -0400

    ran the build

---
+++ client/build/precache-manifest.0d314bb1f8f1f2e5a5bed3ac3d41c3ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26cdf010e29d717d28f131f9d055efc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd77ca48ccb50007c4f3",
+    "url": "/static/css/2.6d803990.chunk.css"
+  },
+  {
+    "revision": "8baf82f3576d9fcbbcfc",
+    "url": "/static/css/main.9335ea4e.chunk.css"
+  },
+  {
+    "revision": "bd77ca48ccb50007c4f3",
+    "url": "/static/js/2.6cb080fb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.6cb080fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8baf82f3576d9fcbbcfc",
+    "url": "/static/js/main.a8108c28.chunk.js"
+  },
+  {
+    "revision": "20c573aaaa2d44c1c4a3",
+    "url": "/static/js/runtime-main.5908aa69.js"
+  }
+]);
